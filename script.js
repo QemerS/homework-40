0cc0917164b35839b844ece6fb5ebad1569b4e93
QemerS <tu201806169@code.edu.az>
2021-12-02 04:36:45 +0400

    hazir

---
+++ script.js
@@ -0,0 +1,38 @@
+// let a = parseInt(prompt("ilk eded"));
+// let b = parseInt(prompt("ikinci eded"));
+// alert("ededlerin cemi " + (a+b));
+// for (let index = 1; index <= 5; index++) {
+//     for (let i = 0; i < index; i++) {
+//        console.log("*"); 
+//     }
+//     console.log(" ");
+// }
+
+// let a = parseInt(prompt("ilk eded"));
+// let b = parseInt(prompt("ikinci eded"));
+// if(a>0 && b<0){
+//     console.log("Ededlerden biri musbet, biri menfidir");
+// } else if(a<0 && b>0){
+//     console.log("Ededlerden biri menfi, biri musbetdir")
+// }
+
+// let eded = prompt("ededi daxil edin: ")
+// let reqem = "";
+// while (eded != 0) {
+//     reqem+= parseInt(eded%10);
+//     eded = parseInt(eded/10);
+// }
+// alert("ededinizin tersi: " + reqem);
+
+// let a = parseInt(prompt("uzunlug"));
+// let b = parseInt(prompt("en"));
+// alert("sahe "+ (a*b));
+
+// let sum=0;
+// for (let index = 1; index < 101; index++) {
+//     if(index%3==0 && index%5==0){
+//         sum+=index;
+//     }
+// }
+// alert(index);
+
